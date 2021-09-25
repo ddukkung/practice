193e4364ed48aaeefddd5eedd1ee31bd2789f769
Jaehee Bae <gjznn3657@gmail.com>
2021-09-25 14:20:15 +0900

    Java Swing - JCheckBox

--- src/practice0924/Practice02.java
+++ src/practice0924/Practice02.java
@@ -35,7 +35,7 @@ public class Practice02 {
 		// 체크박스 체크 또는 체크해제 시 이벤트 처리(ActionListener 사용)
 		// -> 체크박스 각각에 이벤트 연결 필요
 		// 4단계 구현방법을 통해 3개의 체크박스를 동시에 처리
-		ActionListener checkListener = new ActionListener() {
+		ActionListener listener = new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -52,25 +52,25 @@ public class Practice02 {
 					
 					if(cb.isSelected()) { // 체크 여부 판별
 						System.out.println(cb.getActionCommand() + " 체크 됨");
-						
 					} else {
 						System.out.println(cb.getActionCommand() + " 체크 해제됨");
 					}
 					
-					// 체크박스별 수행할 작업이 다를 경우
+				}
+				
+				// 체크박스별 수행할 작업이 다를 경우
 //					if(cb == cb1) {
 //						// 체크1에 대한 작업
 //					} else if(cb == cb2) {
 //						// 체크2에 대한 작업
 //					}
-				}
 			}
 		};
 		
 		// 체크박스에 ActionListener 구현체 연결
-		cb1.addActionListener(checkListener);
-		cb2.addActionListener(checkListener);
-		cb3.addActionListener(checkListener);
+		cb1.addActionListener(listener);
+		cb2.addActionListener(listener);
+		cb3.addActionListener(listener);
 		
 		// 만약, 체크박스별 5단계로 구현할 경우
 		cb1.addActionListener(new ActionListener() {
@@ -84,16 +84,17 @@ public class Practice02 {
 				} else {
 					System.out.println("(5단계) 체크1 체크 해제됨");
 				}
+				
 			}
 		});
 		
 		// JButton 생성 및 SOUTH 영역 부착
 		// 확인 버튼 클릭 시 3개의 체크박스 체크 여부를 판별하여
 		// 체크되어 있는 항목에 대해 "XXX 체크됨" 출력
-		JButton btnCheck = new JButton("확인");
-		f.add(btnCheck, BorderLayout.SOUTH);
+		JButton btn = new JButton("확인");
+		f.add(btn, BorderLayout.SOUTH);
 		
-		btnCheck.addActionListener(new ActionListener() {
+		btn.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -108,8 +109,14 @@ public class Practice02 {
 				if(cb3.isSelected()) {
 					System.out.println("체크3 체크됨");
 				}
+				
+				// if문을 따로 작성하지 않고 else if문을 사용할 경우
+				// cb1을 선택하면 나머지 버튼들이 체크되어도 이미 if문에서 true가 만족되었기 때문에
+				// 밑의 else if문들은 검사를 하지 않게 되므로 체크되어도 출력문이 실행되지 않는다.
+				// 그렇기 때문에 각각 개별로 if문을 작성해야 모두 실행된다.
 			}
 		});
+				
 		
 		f.setVisible(true);
 	}
